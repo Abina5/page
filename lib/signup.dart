4e0001f6cd5cd90a40ee9d604550b7959ff950f1
vaishnav <vaishnavmanu698@gmail.com>
2024-03-21 17:10:45 +0530

    jhh

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:page/login.dart';
 
 class Signup extends StatefulWidget {
   const Signup({super.key});
@@ -14,104 +15,116 @@ class _SignupState extends State<Signup> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-          Center(child: Text('Sign up',style: TextStyle(color: Colors.black,fontSize: 30,fontWeight: FontWeight.bold))),
-    SizedBox(height: 20),
-    Center(child: Text('Create an acccount,Its free',style:TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.w200),)),
-
-
-    SizedBox(height: 40,),
-    Text('Username',style:TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.w200 ,)),
-    TextField(
-    decoration: InputDecoration(
-    enabledBorder: OutlineInputBorder(
-    borderSide: BorderSide(
-    width: 1, color: Colors.black), //<-- SEE HERE
-    ),
-
-
-    ),
-    ),
-
-    SizedBox(height: 40,),
-    Text('Email',style:TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.w200 ,)),
-    TextField(
-    decoration: InputDecoration(
-    enabledBorder: OutlineInputBorder(
-    borderSide: BorderSide(
-    width: 1, color: Colors.black), //<-- SEE HERE
-    ),
-
-
-    ),
-    ),
-
-
-
-    SizedBox(height: 40,),
-    Text('Password',style:TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.w200 ,)),
-    TextField(
-    decoration: InputDecoration(
-    enabledBorder: OutlineInputBorder(
-    borderSide: BorderSide(
-    width: 1, color: Colors.black), //<-- SEE HERE
-    ),
-
-    ),
-    ),
-
-
-    SizedBox(height: 40,),
-    Text('Confirm Password',style:TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.w200 ,)),
-    TextField(
-    decoration: InputDecoration(
-    enabledBorder: OutlineInputBorder(
-    borderSide: BorderSide(
-    width: 1, color: Colors.black), //<-- SEE HERE
-    ),
-
-
-    ),
-    ),
-
-
-    SizedBox(height: 10,),
-    Center(
-    child:
-
-
-    ElevatedButton(onPressed:(){}, child: Text('Sign Up'),),),
-
-    SizedBox(height: 10,),
-    Center(
-    child: Row(
-    children: [Text('Dont  you have an account?Sign up',
-    style: TextStyle(color: Colors.black)),
-    TextButton(onPressed: () {
-
-    },
-    child: const Text('Login')),
-    ]
-
-
-
-    ),
-    ),
-    ],
-    ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+                child: Text('Sign up',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold))),
+            SizedBox(height: 20),
+            Center(
+                child: Text(
+              'Create an acccount,Its free',
+              style: TextStyle(
+                  color: Colors.black, fontSize: 20, fontWeight: FontWeight.w200),
+            )),
+            SizedBox(
+              height: 40,
+            ),
+            Text('Username',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w200,
+                )),
+            TextField(
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(width: 1, color: Colors.black), //<-- SEE HERE
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text('Email',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w200,
+                )),
+            TextField(
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(width: 1, color: Colors.black), //<-- SEE HERE
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text('Password',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w200,
+                )),
+            TextField(
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(width: 1, color: Colors.black), //<-- SEE HERE
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Text('Confirm Password',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w200,
+                )),
+            TextField(
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide:
+                      BorderSide(width: 1, color: Colors.black), //<-- SEE HERE
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Sign Up'),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Center(
+              child: Row(children: [
+                Text('Dont  you have an account?Sign up',
+                    style: TextStyle(color: Colors.black)),
+                TextButton(onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => Login(),));
+                }, child: const Text('Login')),
+              ]),
+            ),
+          ],
+        ),
+      ),
     );
-
-
-
-
-
-
-
-
-
-
-
-
   }
 }
